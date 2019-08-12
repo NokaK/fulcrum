b0ba73aaf18f4a1eeed5d0430005d2d0ef1ce88c
Noka Kipiani <noka.kipiani@wandio.com>
2019-08-12 17:29:44 +0400

    footer responsive update

---
+++ website/local/config.js
@@ -0,0 +1,25 @@
+window.APEXWebConfig = {
+  global: {
+    siteName: 'Fulcrum',
+    theme: 'dark',
+    products: [],
+    instruments: [],
+    gateway: "wss://apifulcrumprod.alphapoint.com/WSGateway/"
+  },
+  Deposit: {
+    hideDepositBankInfo: false,
+    USD: {
+      bankName: 'My USD Bank',
+      bankWireNumber: '1234567890',
+      bankAccountNumber: '123456',
+    },
+    ZAR: {
+      bankName: 'My ZAR Bank',
+      bankWireNumber: '1234567890',
+      bankAccountNumber: '123456',
+    },
+  },
+  Settings: {
+    demoMode: false
+  },
+};
